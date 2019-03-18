92d870829745a4de02328e0fcff530c20223b0f0
ashrek1 <agoodal1999@gmail.com>
2019-03-18 18:21:51 +0200

    fixed factorial

--- mypackage/recursion.py
+++ mypackage/recursion.py
@@ -18,7 +18,7 @@ def factorial(n):
     if n == 1:
         return n
     elif n == 0:
-        return 0
+        return 1
     else:
         lower_fact = factorial(n-1)
         current_fact = n * lower_fact
